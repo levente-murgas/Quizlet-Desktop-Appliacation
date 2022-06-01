a947662a30b101b949544cc5b0c97554c5e20c44
Levente Murgas <murgaslevi@gmail.com>
2022-06-01 20:12:57 +0200

    App class was created to separate argument initializations and app running logic.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -199,60 +199,9 @@ fun resetStats(){
     logger.logOutput("Card statistics have been reset.")
 }
 
-fun initApp(args: Array<String>){
-    if (args.isNotEmpty()) {
-        if(args.size == 2) { //only import or export
-            when(args[0]) {
-                "-import" -> {
-                    importFileName = args[1]
-                    importCards()
-                }
-                "-export" -> exportFileName = args[1]
-            }
-        }
-        else if(args.size == 4) { //import AND export
-            when(args[0]) {
-                "-import" -> {
-                    importFileName = args[1]
-                    importCards()
-                }
-                "-export" -> exportFileName = args[1]
-            }
-            when(args[2]) {
-                "-import" -> {
-                    importFileName = args[3]
-                    importCards()
-                }
-                "-export" -> exportFileName = args[3]
-            }
-        }
-    }
-}
 
 fun main(args: Array<String>) {
-    initApp(args)
-
-    var exit = false
-
-    while(!exit) {
-        logger.logOutput("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):")
-        when(logger.logInput()){
-            "add" -> addCard()
-            "remove" -> removeCard()
-            "import" -> importCards()
-            "export" -> exportCards()
-            "ask" -> askCard()
-            "exit" -> {
-                exit = true
-                logger.logOutput("Bye bye!")
-                exportCards(exiting = true)
-            }
-            "log" -> logger.saveLog()
-            "hardest card" -> hardestCardFunc()
-            "reset stats" -> resetStats()
-            else -> {
-                logger.logOutput("Not an action name!")
-            }
-        }
-    }
+    val app = App()
+    app.initApp(args)
+    app.runApp()
 }
